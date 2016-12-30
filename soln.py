4cb26cf88439a0191706ee353f28ae1dbb0a9848
Pralave <pralave.94@outlook.com>
2016-12-30 12:32:10 +0530

    new condition added for numbers

--- soln.py
+++ soln.py
@@ -9,8 +9,8 @@ def get_num(filename):
 			for line in f: ## read every textfile line by line
 				line_list = line.split() ## covert all words in a line to list item
 				for item in line_list:
-					item = item.replace("+91", "") ## remove +91 from the beginning of a number for example "+918290093302 = 8290093302"
-					item = item.replace("-", "")
+					item = item.replace("+91", "").replace("-", "") ## remove +91 from the beginning of a number for example "+918290093302 = 8290093302"
+					item = item.replace("(", "").replace(")", "")
 					if len(item) == 10 and item.isdigit():
 						if item.startswith("7") or item.startswith("8") or item.startswith("9"): ## indian Mobile numbers startswith 7, 8 or 9 
 							text_file.write("\n%s\n" %item) ## write the numbers in output.txt
@@ -25,6 +25,6 @@ if __name__ == "__main__":
 	except Exception as e:
 		pass
 
-	map(get_num, glob.glob(os.path.join(path, "test_directory/*.txt"))) ## map every file to get_num() function to get all the numbers.
+	map(get_num, glob.glob(os.path.join(path, "test_directory/*.txt"))) ## map every text file to get_num() function to get all the numbers.
 
 
